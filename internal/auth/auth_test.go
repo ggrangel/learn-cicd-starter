459425b9b5c7eb4266d8815937d47b0b9bbe266e
Gustavo Rangel <gustavorangel91@gmail.com>
2024-06-29 08:45:58 -0300

    Run unit tests as a CI step

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,30 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	tests := []struct {
+		input   http.Header
+		wantStr string
+		wantErr error
+	}{
+		{
+			input:   http.Header{"Authorization": {"ApiKey 12345"}},
+			wantStr: "1234",
+			wantErr: nil,
+		},
+	}
+
+	for _, tc := range tests {
+		gotStr, gotErr := GetAPIKey(tc.input)
+		if gotStr != tc.wantStr {
+			t.Fatalf("GetAPIKey(%v) = %v, want %v", tc.input, gotStr, tc.wantStr)
+		}
+		if gotErr != tc.wantErr {
+			t.Fatalf("GetAPIKey(%v) = %v, want %v error", tc.input, gotErr, tc.wantErr)
+		}
+	}
+}
